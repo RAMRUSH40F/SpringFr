124080da17d521d092340a6a6eb4b7875f89b550
ramru <ramrush-r@mail.ru>
2022-12-03 23:53:30 +0300

    reformat

--- src/main/java/Annotations/Test.java
+++ src/main/java/Annotations/Test.java
@@ -7,6 +7,11 @@ public class Test
     public static void main(String[] args)
 
     {
+        int b = '0';
+        int result = 1/b;
+        System.out.println(b);
+        System.out.println(result);
+
         // Спринг прочитал аппликейшн котнтекст, после чего можно вызвать бин
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
