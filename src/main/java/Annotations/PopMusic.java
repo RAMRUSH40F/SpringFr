97700ad7ebe3b943750dd27ff3ba1f3579aa514d
ramru <ramrush-r@mail.ru>
2023-01-06 15:39:08 +0300

    + planned refactoring code after 3 months

--- src/main/java/Annotations/PopMusic.java
+++ src/main/java/Annotations/PopMusic.java
@@ -3,21 +3,20 @@ package Annotations;
 import org.springframework.stereotype.Component;
 
 @Component("popMusic")
-public class PopMusic implements Music
-{
+public class PopMusic implements Music {
 
-    private final String[] list = {"Metallica", "40 miles", "Zavtra Broshu"};
-    private String name = "Despacito";
+    private final String[] list = { "Metallica", "40 miles", "Zavtra Broshu" };
+    private       String   name = "Despacito";
 
     public void setName(String name) {
         this.name = name;
     }
 
     public String[] getSongList() {
-        return list ;
+        return list;
     }
-    @Override
 
+    @Override
     public String getSong() {
         return name;
     }
