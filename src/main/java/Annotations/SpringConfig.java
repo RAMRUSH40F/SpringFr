5560bf3cf368223b0f1da3d983c8747468d9c541
ramru <ramrush-r@mail.ru>
2023-01-06 15:39:25 +0300

    + planned refactoring code after 3 months

--- src/main/java/Annotations/SpringConfig.java
+++ src/main/java/Annotations/SpringConfig.java
@@ -7,7 +7,5 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration
 @ComponentScan("Annotations")
 @PropertySource("classpath:musicPlayer.properties")
-public class SpringConfig
-{
-
+public class SpringConfig {
 }
