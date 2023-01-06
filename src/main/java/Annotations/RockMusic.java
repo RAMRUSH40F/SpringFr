6106f77a55e1a9f1d0aa30c7f04bab03d180691f
ramru <ramrush-r@mail.ru>
2023-01-06 15:39:17 +0300

    + planned refactoring code after 3 months

--- src/main/java/Annotations/RockMusic.java
+++ src/main/java/Annotations/RockMusic.java
@@ -1,24 +1,22 @@
 package Annotations;
 
 import org.springframework.stereotype.Component;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 @Component("rockMusic")
-public class RockMusic implements Music
-{
+public class RockMusic implements Music {
 
     @PostConstruct
-    public void doMyinit(){
+    public void doMyinit() {
         System.out.println("RockMusicClass: Doing my initialization");
     }
 
     @PreDestroy
-    public void doMyDestroy(){
+    public void doMyDestroy() {
         System.out.println("RockMusicClass: Doing my destroyment");
     }
-
-
 
     @Override
     public String getSong() {
